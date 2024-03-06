c4efb624bf751c2c9d1db518778d010c5025e69f
LMWTT1230 <willkang0131@gmail.com>
2024-03-05 23:12:53 -0800

    more stuff

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CollisionHandler : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D other) {
-        if(other.gameObject.name == "Player(Placeholder)"){
+        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Bullet")){
             Vector2 rand = new Vector2(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f));
             transform.position = rand;
         }
