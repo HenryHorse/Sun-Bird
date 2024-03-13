3941d7bc2c42830b064e0fd7f9b637687686c7df
Shayan Daijavad <shayan@daijavad.com>
2024-03-13 14:07:07 -0700

    added damage animations

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -7,12 +7,18 @@ public class CollisionHandler : MonoBehaviour
     private HealthBar playerHealthBar;
     public float damageAmount = 20f;
     public float enemyHealth = 20f;
+    private SpriteRenderer spriteRenderer;
 
+
+    void Start ()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.CompareTag("Player")) 
         {
             playerHealthBar = other.gameObject.GetComponent<HealthBar>();
-            playerHealthBar.currentHealth -= damageAmount;
+            playerHealthBar.TakeDamage(damageAmount);
             Destroy(gameObject);
         }
         else if (other.gameObject.CompareTag("Bullet"))
@@ -20,9 +26,17 @@ public class CollisionHandler : MonoBehaviour
             // Vector2 rand = new Vector2(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f));
             // transform.position = rand;
             enemyHealth -= 5f;
+            StartCoroutine(DamageFlash());
             if (enemyHealth <= 0) {
                  Destroy(gameObject);
             }
         }
+    }
+
+    IEnumerator DamageFlash() 
+    {
+        spriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(0.2f);
+        spriteRenderer.color = Color.white;
     }
 }
--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
+
 
 public class HealthBar : MonoBehaviour
 {
@@ -10,10 +13,13 @@ public class HealthBar : MonoBehaviour
     public Slider healthSlider;
     public float maxHealth = 100f;
     public float currentHealth;
+
+    public SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -27,5 +33,20 @@ public class HealthBar : MonoBehaviour
         {
             SceneManager.LoadScene(1);
         }
+    }
+
+
+    public void TakeDamage(float damageAmount) 
+    {
+        currentHealth -= damageAmount;
+        StartCoroutine(DamageFlash());
+    }
+
+
+    IEnumerator DamageFlash() 
+    {
+        spriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(0.2f);
+        spriteRenderer.color = Color.white;
     }
 }
