b34b0ce4bc4ecacc2e4d22422ae25ac67e3fdf62
EmuMan <jacob.kelleran@outlook.com>
2024-03-20 08:17:20 -0700

    Fixed line of code preventing builds

--- Assets/Scripts/Player/PlayerAbilities.cs
+++ Assets/Scripts/Player/PlayerAbilities.cs
@@ -4,7 +4,6 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
-using static UnityEditor.FilePathAttribute;
 
 public class PlayerAbilities : MonoBehaviour
 {
